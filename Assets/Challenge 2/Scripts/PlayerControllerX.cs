addf8c6dfb27e08616fab7d93d18dee7945317cb
Raheel Ahmed <raheel.6477889@talmeez.pk>
2022-02-07 12:49:59 +0500

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spamTimer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time>=spamTimer)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spamTimer = Time.time + 2;
+        }
+    }
+}
